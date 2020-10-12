8bab73f03997c1e00798cd1e04f437393da11375
llxqb <llxqb_tt@126.com>
2020-10-12 15:52:01 +0800

    测试分支 提交

--- app/src/main/java/com/s20cxq/testgit/MainActivity.kt
+++ app/src/main/java/com/s20cxq/testgit/MainActivity.kt
@@ -3,6 +3,9 @@ package com.s20cxq.testgit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 我是测试分支
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
