15306d37933b700c1d825eafc986c183d5f8e890
llxqb <llxqb_tt@126.com>
2020-10-12 15:50:44 +0800

    master 提交

--- app/src/main/java/com/s20cxq/testgit/MainActivity.kt
+++ app/src/main/java/com/s20cxq/testgit/MainActivity.kt
@@ -3,9 +3,13 @@ package com.s20cxq.testgit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 我是master分支
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //新增内容
     }
 }
